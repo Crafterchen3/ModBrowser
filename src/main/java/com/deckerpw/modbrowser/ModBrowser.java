1dcfdc1f940f26bced4822b2cfde4ab9911c07e6
Crafterchen3 <deckerpw@gmail.com>
2026-04-24 22:56:38 +0200

    Initial Neoforge setup

--- src/main/java/com/deckerpw/modbrowser/ModBrowser.java
+++ src/main/java/com/deckerpw/modbrowser/ModBrowser.java
@@ -1,150 +1,117 @@
 package com.deckerpw.modbrowser;
 
-import com.deckerpw.modbrowser.gui.ModBrowserMainMenuScreen;
-import com.deckerpw.modbrowser.gui.SetupScreen;
-import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
-import com.mojang.authlib.yggdrasil.YggdrasilEnvironment;
-import com.mojang.authlib.yggdrasil.YggdrasilMinecraftSessionService;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.TitleScreen;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.client.event.ScreenOpenEvent;
-import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(ModBrowser.MOD_ID)
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(ModBrowser.MODID)
 public class ModBrowser {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "modbrowser";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+    // Create a Deferred Register to hold Blocks which will all be registered under the "modbrowser" namespace
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+    // Create a Deferred Register to hold Items which will all be registered under the "modbrowser" namespace
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "modbrowser" namespace
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    public final static String MOD_ID = "modbrowser";
-    public final static String MC_VERSION = "1.18.2";
-    public final static int GHOST_ID = -54;
+    // Creates a new Block with the id "modbrowser:example_block", combining the namespace and path
+    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
+    // Creates a new BlockItem with the id "modbrowser:example_block", combining the namespace and path
+    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
 
-    public static ModIndex index;
+    // Creates a new food item with the id "modbrowser:example_id", nutrition 1 and saturation 2
+    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
+            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
 
-    public ModBrowser() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::loadComplete);
+    // Creates a creative tab with the id "modbrowser:example_tab" for the example item, that is placed after the combat tab
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.modbrowser")) //The language key for the title of your CreativeModeTab
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
+            }).build());
 
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT,ModBrowserConfigs.SPEC,"modbrowser-client.toml");
+    // The constructor for the mod class is the first code that is run when your mod is loaded.
+    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+    public ModBrowser(IEventBus modEventBus, ModContainer modContainer) {
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register the Deferred Register to the mod event bus so blocks get registered
+        BLOCKS.register(modEventBus);
+        // Register the Deferred Register to the mod event bus so items get registered
+        ITEMS.register(modEventBus);
+        // Register the Deferred Register to the mod event bus so tabs get registered
+        CREATIVE_MODE_TABS.register(modEventBus);
+
+        // Register ourselves for server and other game events we are interested in.
+        // Note that this is necessary if and only if we want *this* class (ModBrowser) to respond directly to events.
+        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+        NeoForge.EVENT_BUS.register(this);
+
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
+
+        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
-        // Some preinit code
-        System.out.println("USER: "+ Minecraft.getInstance().getUser().getSessionId());
-        Minecraft mc = Minecraft.getInstance();
-        try {
-            index = new ModIndex(Minecraft.getInstance());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private void commonSetup(FMLCommonSetupEvent event) {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+
+        if (Config.LOG_DIRT_BLOCK.getAsBoolean()) {
+            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
         }
 
+        LOGGER.info("{}{}", Config.MAGIC_NUMBER_INTRODUCTION.get(), Config.MAGIC_NUMBER.getAsInt());
 
+        Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
     }
 
-    private void loadComplete(final FMLLoadCompleteEvent event)
-    {
-   }
-
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-        // Some example code to dispatch IMC to another mod
-    }
-
-    private void processIMC(final InterModProcessEvent event) {
-        // Some example code to receive and process InterModComms from other mods
+    // Add the example block item to the building blocks tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(EXAMPLE_BLOCK_ITEM);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
         // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
     }
-
-    public enum ModType {
-        MODS("/mods", 6),
-        RESOURCE_PACKS("/resourcepacks", 12),
-        FILES("",0);
-        public String prefix;
-        int classId;
-
-        ModType(String prefix, int classId) {
-            this.prefix = prefix;
-            this.classId = classId;
-        }
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // Register a new block here
-        }
-    }
-
-    @SubscribeEvent
-    public void onMainMenu(ScreenOpenEvent e) {
-        if (e.getScreen() != null && e.getScreen().getClass() == TitleScreen.class) {
-            if (ModBrowserConfigs.FIRST_START.get())
-                e.setScreen(new SetupScreen(new TranslatableComponent("setup.name"),new ModBrowserMainMenuScreen(true)));
-            else{
-                if (ModBrowser.ModBrowserConfigs.AUTO_UPDATE_BEHAVIOR.get() != AutoUpdate.AutoUpdateBehavior.NO_AUTO_UPDATE) new AutoUpdate(Minecraft.getInstance()).updateMods();
-                e.setScreen(new ModBrowserMainMenuScreen(true));
-            }
-
-        } else {
-            e.setResult(Event.Result.ALLOW);
-        }
-    }
-
-
-
-    public class ModBrowserConfigs {
-        public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
-        public static final ForgeConfigSpec SPEC;
-        public static final ForgeConfigSpec.ConfigValue<String> REFRESH_TOKEN;
-        public static final ForgeConfigSpec.ConfigValue<Boolean> SHOW_PROHIBITED_MODS;
-        public static final ForgeConfigSpec.ConfigValue<Boolean> FIRST_START;
-
-        public static final ForgeConfigSpec.ConfigValue<AutoUpdate.AutoUpdateBehavior> AUTO_UPDATE_BEHAVIOR;
-
-        static {
-            BUILDER.push("Configs for Mod Browser");
-            REFRESH_TOKEN = BUILDER.define("Refresh Token","");
-            SHOW_PROHIBITED_MODS = BUILDER.define("Show prohibited mods",false);
-            FIRST_START = BUILDER.define("first start",true);
-            AUTO_UPDATE_BEHAVIOR = BUILDER.defineEnum("Automatic search/install updates", AutoUpdate.AutoUpdateBehavior.NO_AUTO_UPDATE);
-            BUILDER.pop();
-            SPEC = BUILDER.build();
-        }
-
-    }
-
-
 }
